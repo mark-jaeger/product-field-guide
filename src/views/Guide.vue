3e630a622cb6ad01e14d3f646df096759c268911
Mark Jäger <mark@jaeger.digital>
2018-06-26 18:55:35 +0200

    loading step content from prismic

--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="guide">
-    <select v-model="currentStepId">
-      <option
-       v-bind:key="step.data.id" 
-       v-for="step in steps" 
-       v-bind:value="step.id">
-        {{ PrismicDOM.RichText.asText(step.data.title) }}
-      </option>
-    </select>
+    <section class="instructions">
+      <nav>
+        <select 
+          v-model="currentStepUid"
+          v-on:change="changeStep">
+          <option
+          v-bind:key="step.data.uid" 
+          v-for="step in steps" 
+          v-bind:value="step.uid">
+            {{ PrismicDOM.RichText.asText(step.data.title) }}
+          </option>
+        </select>
+      </nav>
+      <section v-if="currentStep">
+        <headline>{{ PrismicDOM.RichText.asText(currentStep.data.title) }}</headline>
+        <div v-html="PrismicDOM.RichText.asHtml(currentStep.data.description)"></div>
+      </section>
+    </section>
+    <section class="editor">
+      <iframe class="editorFrame" src="https://docs.google.com/drawings/d/1x5Ic4oNpAdhoDqi-6TD6yZX0CnTSStxNEALlTFqHSEg/copy"></iframe>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Headline from '@/components/Headline.vue';
+
 const Prismic = require('prismic-javascript')
 const PrismicDOM = require('prismic-dom')
-const currentStepId = null
 
 export default {
   name: 'home',
@@ -24,7 +38,8 @@ export default {
       Prismic,
       PrismicDOM,
       steps: [],
-      currentStepId
+      currentStepUid: null,
+      currentStep: null
     };
   },
   methods: {
@@ -36,15 +51,44 @@ export default {
         { orderings: '[my.step.step_number]' },
       )).then((response) => {
         $this.steps = response.results;
-        this.currentStepId = response.results[0].id
+        this.setStep(response.results[0].uid)
       }, (err) => {
         console.log('Something went wrong: ', err);
         return [];
       });
     },
+    setStep(stepUid) {
+      this.currentStepUid = stepUid
+
+      Prismic.getApi(this.$prismicApiEndpoint, { accessToken: this.$prismicAccessToken }).then(api => api.getByUID(
+        'step', stepUid
+      )).then((response) => {
+        this.currentStep = response
+      }, (err) => {
+        console.log('Something went wrong: ', err);
+        return [];
+      });
+    },
+    changeStep() {
+      this.setStep(this.currentStepUid)
+    }
+  },
+  components: {
+    headline: Headline
   },
   beforeMount() {
     this.loadSteps();
   },
 };
 </script>
+<style lang="sass" scoped>
+.guide
+  display: flex
+  flex-wrap: nowrap
+  justify-content: space-between
+  align-items: stretch
+  align-content: stretch
+
+.instructions
+
+</style>

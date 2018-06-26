e51433cad6cc0ebe6e1b775ea37de301b8257892
Mark Jäger <mark@jaeger.digital>
2018-06-26 15:31:59 +0200

    added product field home

---
+++ src/components/Headline.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2><slot></slot></h2>
+</template>
+
+<script>
+export default {
+  name: 'Headline'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+h2
+    font-size: 48px
+    color: blue
+
+h3
+    font-size: 24px
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <headline>
+        Product Field<br>
+        Step-by-Step Guide
+    </headline>
+    <p>This Online guide takes you from blank canvas to understanding your product step-by-step.</p>
+    <p>The guide usually takes about <strong>2 hours</strong>.</p>
+    <p>There is no registration necessary. But if you want to pause and come back, we can send you a link that gets you to where you left off.</p>
+    <p><strong>Resume link via E-Mail:</strong></p>
+    <div><input class="email" v-model="email" placeholder="E-Mail"/></div>
+    <div>
+        <button v-on:click="start">Start now</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Headline from '../components/Headline.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  props: {
+    email: "",
+    subscribe: true
   },
-};
+  components: {
+    headline: Headline
+  },
+  methods: {
+    start: function(){
+      this.$router.push('guide');
+    }
+  }
+}
 </script>
+
+<style scoped lang="sass">
+.email
+  width: 100%
+</style>

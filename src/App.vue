0461b6dada6b9a6ee087b4cf71a1b46f5073e8be
Mark Jäger <mark@jaeger.digital>
2018-06-26 16:19:37 +0200

    added general Layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+<script>
+</script>
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Cabin:400,700')
+@import url("https://use.typekit.net/vqa3zmx.css")
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+$primary-color: #304FFE
+
+#app
+  font-family: 'Cabin', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  padding: 32px
+  line-height: 1.333
+  font-size: 24px
+
+h1,h2,h3,h4,h5,h6,h7,h8
+  font-family: 'museo-slab', Helvetica, Arial, sans-serif
+  font-weight: bold
+  line-height: 1
+  color: $primary-color
+  padding: 0
+  margin: 0
+
+input[type="text"], input[type="email"], input[type="password"], input[type="search"]
+  padding: 12px
+  box-sizing: border-box
+
+#nav
+  padding: 30px
+  a
+    font-weight: bold
+    color: #2c3e50
+    &.router-link-exact-active
+      color: #42b983
+
 </style>
